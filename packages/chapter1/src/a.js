53ea3efb1e17800343e4ec28e32610965cae73b4
devjaewon <devjaewon21@gmail.com>
2024-03-07 17:48:11 +0900

    feat(chapter1): 문제 A 해설 보완 (no-issue)

--- packages/chapter1/src/a.js
+++ packages/chapter1/src/a.js
@@ -20,9 +20,13 @@ Worker.prototype.work = function () {
   this._health--;
 }
 
+// Object.create을 사용했는데, setPrototypeOf를 사용해도 되고 new 키워드를 사용해도 무방합니다. (new 키워드는 불필요한 처리가 추가로 들어가게 되겠지만요)
+// 프로토타입 슬롯을 갖는 객체를 prototype에 할당함으로써 체이닝을 구성할 수 있다는 점만 이해하면 됩니다.
 JuniorEngineer.prototype = Object.create(Worker.prototype, {});
 
 JuniorEngineer.prototype._super = function (health) {
+  // 부모 함수를 반영하기 위해 call 함수를 이용하고 있습니다.
+  // 이 로직을 이해하기 위해서는 자바스크립트 함수의 동작과 this 바인딩 개념을 알아야 합니다.
   Worker.call(this, health);
 }
 
@@ -31,6 +35,8 @@ JuniorEngineer.prototype.getIntelligence = function () {
 }
 
 JuniorEngineer.prototype.work = function () {
+  // 만약 메서드 명이 달랐다면 this 키워드로 체이닝을 통해 바로 접근이 가능 했겠지만
+  // 메서드 명이 같아서 아래와 같이 실행해줄 수 있습니다.
   Worker.prototype.work.call(this);
   this._intelligence++;
 }
